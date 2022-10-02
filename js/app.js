eea0ac975feb7bc5aad43570357e206875ad54f8
Youssef Girgeis <youssef_girgeis@hotmail.com>
2022-10-02 17:52:01 -0400

    create navbar dynamically

--- js/app.js
+++ js/app.js
@@ -1,62 +1,69 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
+const navbarList = document.getElementById('navbar__list');
+const sections = document.querySelectorAll('.landing__container');
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
+const createNavbar = () => {
+  for (let i = 0; i < sections.length; i++) {
+    const listItem = document.createElement('li');
+    const menuLink = document.createElement('a');
+    menuLink.classList.add('menu__link');
+    menuLink.textContent = sections[i].firstElementChild.textContent;
+    listItem.appendChild(menuLink);
+    navbarList.appendChild(listItem);
+  }
+};
 
+createNavbar();
 
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
